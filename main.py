1b9592649cafb014efa1e6f4fa518c6cad147bb4
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2023-08-22 23:40:07 -0300

    FINAL

--- main.py
+++ main.py
@@ -3,52 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Carregar a imagem 
-imagem = cv2.imread('atletico.png', cv2.IMREAD_COLOR)
+imagem = cv2.imread('gremio.png', cv2.IMREAD_COLOR)  # Altere 'gremio.png' para o nome do seu arquivo de imagem
 
 # Converter para tons de cinza
 imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-# Gerar o histograma da imagem de tons de cinza
 histograma_original = cv2.calcHist([imagem], [0], None, [256], [0, 256])
 
-# Gerar o histograma da imagem em tons de cinza
 histograma_cinza = cv2.calcHist([imagem_cinza], [0], None, [256], [0, 256])
 
-# Equalizar o histograma da imagem em tons de cinza
 imagem_equalizada = cv2.equalizeHist(imagem_cinza)
 
-# Gerar o histograma da imagem equalizada
 histograma_equalizado = cv2.calcHist([imagem_equalizada], [0], None, [256], [0, 256])
 
-# Plotar as imagens e histogramas em ordem: imagem, histograma, imagem, histograma
-plt.figure(figsize=(12, 6))
-
-# Imagem Original
+plt.figure(figsize=(12, 6),num='GREMIO > ATLETICO')
 plt.subplot(2, 4, 1)
 plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
 plt.title('Imagem Original')
 
-# Histograma Original
 plt.subplot(2, 4, 2)
 plt.plot(histograma_original)
 plt.title('Histograma Original')
 
-# Imagem em Tons de Cinza
 plt.subplot(2, 4, 3)
 plt.imshow(imagem_cinza, cmap='gray')
 plt.title('Imagem em Tons de Cinza')
 
-# Histograma em Tons de Cinza
 plt.subplot(2, 4, 4)
 plt.plot(histograma_cinza)
 plt.title('Histograma Tons de Cinza')
 
-# Imagem Equalizada
 plt.subplot(2, 4, 5)
 plt.imshow(imagem_equalizada, cmap='gray')
 plt.title('Imagem Equalizada')
 
-# Histograma Equalizado
 plt.subplot(2, 4, 6)
 plt.plot(histograma_equalizado)
 plt.title('Histograma Equalizado')
